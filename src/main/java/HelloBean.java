6f7dcd614914d9fdd11f66a3b359aef4b6aa006f
storres1987 <torres.garces.sheila@gmail.com>
2015-02-09 12:14:08 +0100

    Creacion de proyecto con JavaServerFaces

---
+++ src/main/java/HelloBean.java
@@ -0,0 +1,30 @@
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+	//private static final long serialVersionUID = 1L;
+	private String name;
+	private Date date;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+}
